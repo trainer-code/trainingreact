f29f92adbd0848510d1d63105010e69558addcdb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-29 17:16:19 +0000

    Deploy trainer-code/trainingreact to github.com/trainer-code/trainingreact.git:gh-pages

---
+++ precache-manifest.a634707673ee0451e7119836573f3f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acaa6ec5e7af122a5fab2615951fb94",
+    "url": "/trainingreact/index.html"
+  },
+  {
+    "revision": "2a1b22bceec3aaf29c2d",
+    "url": "/trainingreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "90ebbef3e61826c0566d",
+    "url": "/trainingreact/static/js/2.ad199184.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/trainingreact/static/js/2.ad199184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1b22bceec3aaf29c2d",
+    "url": "/trainingreact/static/js/main.5d7ae03e.chunk.js"
+  },
+  {
+    "revision": "59164d75eaeb4533aa56",
+    "url": "/trainingreact/static/js/runtime-main.7f187054.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trainingreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
